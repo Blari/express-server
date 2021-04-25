cad3cefefba8ccca7d7aaf5ff20d9a5b780a512c
Blari <Blarior@gmail.com>
2021-04-25 20:16:29 +0300

    Add pug template

--- routs/admin.js
+++ routs/admin.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
-const rootDir = require('../util/path');
 
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product')
 });
 
 router.post('/add-product', (req, res, next) => {
